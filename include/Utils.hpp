df0e2a1c58b5a372f3aae9e834be189fed06b9e7
Gabriel Menezes <gabrielmenezesvi@gmail.com>
2016-06-18 16:45:22 -0300

    não dava pra deletar a função make_unique

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -47,3 +47,40 @@ inline void tokenize(const std::string &str, ContainerT &tokens,
         lastPos = pos + 1;
     }
 }
+
+
+//implementação de make_unique, tirado de http://stackoverflow.com/a/17902439/482238
+namespace std {
+    template<class T>
+    struct _Unique_if {
+        typedef unique_ptr<T> _Single_object;
+    };
+
+    template<class T>
+    struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Unknown_bound;
+    };
+
+    template<class T, size_t N>
+    struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+
+    template<class T, class... Args>
+    typename _Unique_if<T>::_Single_object
+    make_unique(Args &&... args) {
+        return unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+
+    template<class T>
+    typename _Unique_if<T>::_Unknown_bound
+    make_unique(size_t n) {
+        typedef typename remove_extent<T>::type U;
+        return unique_ptr<T>(new U[n]());
+    }
+
+    template<class T, class... Args>
+    typename _Unique_if<T>::_Known_bound
+    make_unique(Args &&...) = delete;
+    //teste
+}
